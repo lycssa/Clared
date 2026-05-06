2a6c2e1f80084d3de0d31b1d7c4c2291328cdd1e
June <lycssa@gmail.com>
2026-05-06 02:35:06 -0700

    Initial working version

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Clared
+
 Small mod to clean up Minecraft's title screen.
+
+#### Notes
+
+1. This may over-clean, including things from other mods.
+2. Any remaining artifacts should be removable with a custom resourcepack.
 
 Made for Minecraft version 1.21.11
---
+++ src/main/java/clared/mixin/TitleScreenMixin.java
@@ -0,0 +1,27 @@
+package clared.mixin;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin extends Screen {
+
+    protected TitleScreenMixin(Component title) {
+        super(title);
+    }
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void onRender(
+            GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo info) {
+        this.renderPanorama(guiGraphics, partialTick);
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        info.cancel();
+    }
+}
